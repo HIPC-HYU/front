cd283db7267d6e41e389ad0153e191e4e6b9a265
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-07-14 17:06:11 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Main from './pages/Main'
+import About from './pages/About'
+import RankPage from './pages/Rankpage'
+import Navbar from './components/ui/Navbar'
+import Footer from './components/ui/Footer'
+function App() {
+  
+  return (
+    <>    
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/rank' element={<RankPage />}/>
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
