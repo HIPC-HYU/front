5a2e7cbb2211e250dfb099cc7e9a44c905ee0e50
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-08-08 23:54:10 +0900

    2ndversion commit

--- src/pages/Rankpage.tsx
+++
@@ -1,7 +0,0 @@
-import MainRank from "../components/rank/MainRank"
-
-export default function RankPage() {
-    return (<div className="w-full flex-1 mx-auto px-4 pt-20">
-        <MainRank />
-    </div>)
-}
